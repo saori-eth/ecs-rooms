ddc780d704d0c7dddfee759e1c1fc75b10b10a52
saori-eth <d84da@proton.me>
2025-07-15 10:30:40 -0500

    source room descriptsions from room definitions

--- client/ecs/rooms/room-definitions.js
+++ client/ecs/rooms/room-definitions.js
@@ -1,6 +1,7 @@
 export const rooms = {
   "arena-empty": {
-    name: "Empty",
+    name: "Empty Room",
+    description: "Testing purposes",
     camera: "basic",
     sceneModel: "/environments/arena.glb",
     script: "/rooms/scripts/arena-logic.js",
@@ -12,22 +13,12 @@ export const rooms = {
   },
   midship: {
     name: "Midship",
+    description: "A room with a script running",
     camera: "tps",
     sceneModel: "/environments/midship.glb",
     script: "/rooms/scripts/particle-box-scene.js",
     sceneTransform: {
       position: { x: 0, y: 5, z: 0 },
-      rotation: { x: 0, y: 0, z: 0 },
-      scale: { x: 1, y: 1, z: 1 },
-    },
-  },
-  "particle-box": {
-    name: "Particle Box",
-    camera: "basic",
-    sceneModel: "/environments/arena.glb", // Using arena as the box environment
-    script: "/rooms/scripts/particle-box-scene.js",
-    sceneTransform: {
-      position: { x: 0, y: -1, z: 0 },
       rotation: { x: 0, y: 0, z: 0 },
       scale: { x: 1, y: 1, z: 1 },
     },
